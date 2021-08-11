1a7887e662d5aebcc7da1b4c2ebdbe53364171f0
g0pher98 <mail@jseung.me>
2021-08-12 00:38:14 +0900

    1차 완성
    소망가방리스트에서 아직 수강신청이 확정되지 않은 목록을 자동으로
    수강신청함.
    
    아직 문제가 남아있는데, 소망가방 데이터가 영어로 날라옴. 이에
    따라 재수강 여부 필드가 없는데, 일단 'N'으로 고정해둠.

---
+++ macro.py
@@ -0,0 +1,177 @@
+import requests
+import time
+import json
+import re
+
+from requests.models import LocationParseError
+
+class Macro():
+    def __init__(self, profile_path = "./profile.json", host = "http://sugang.kyonggi.ac.kr", debug = False):
+        '''
+        debug: all, login, basket, req
+        '''
+        self.initial_time = self.get_time(forward = 30)
+        self.session = requests.Session()
+        self.debug = debug
+        self.host = host
+        self.referer = ''
+        self.url = {
+            'login': '{}/login?attribute=loginChk&lang={}&fake={}&callback=jQuery112401948917635895624_1628649765381',
+            'basket_list': '{}/sugang?attribute=sugangBasketListJson&lang={}&fake={}&_search={}&nd={}&rows={}&page={}&sidx={}&sord={}',
+            'sugang_list': '{}/sugang?attribute=sugangListJson&lang={}&fake={}&_search={}&nd={}&rows={}&page={}&sidx={}&sord={}',
+            'sugang_mode': '{}/sugang?attribute=sugangMode&lang={}&fake={}&mode={}&fake={}'
+        }
+        self.blacklist = []
+
+        # 로그인 정보 로드
+        with open(profile_path, 'r') as fp:
+            self.profile = json.load(fp)
+
+        return
+
+    def login(self):
+        '''
+        경기대학교 수강신청 홈페이지에 로그인하는 함수
+        '''
+        url = self.url['login'].format(self.host, self.profile['lang'], self.initial_time)
+        self.debug in ['all', 'login'] and print(f"[DEBUG] {url}")
+
+        resp = self.session.post(url, data=self.profile)
+
+        # 응답 데이터 중 실질적으로 필요한 부분만 파싱 후 처리
+        result = re.findall('\{.*\}', resp.text)
+        if result:
+            json_result = json.loads(result[0])
+            if json_result['code'] == '1':
+                print(f"[*] login: {json_result['msg']}")
+            else:
+                print(f"[!] login: {json_result['msg']}")
+                return False
+        else:
+            print('[!] login: nothing in response')
+            return False
+
+        # sync
+        resp = self.session.get(f"{self.host}/core?attribute=coreFrame_{self.profile['lang']}&fake={self.get_time()}")
+        links = re.findall("\/core\?attribute=core[a-zA-Z0-9_=:/.?& ]+", resp.text)
+        if links and len(links) >= 2:
+            if "coreMain" in links[0]:
+                self.referer = self.host + links[0]
+            elif "coreMain" in links[1]:
+                self.referer = self.host + links[1]
+
+        return True
+    
+    def load_basket_list(self):
+        '''
+        수강신청을 위해 소망가방 리스트를 가져오는 함수
+        '''
+        # sync_url = "{}/core?attribute=lectListJson&lang={}&fake={}&menu=2&initYn=N&div_cd=C&_search=false&nd={}&rows=-1&page=1&sidx=&sord=asc".format(self.host, self.profile['lang'], self.initial_time, self.get_time())
+
+        # self.session.get(sync_url, headers = {
+        #     'Referer': self.referer,
+        #     'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,und;q=0.6'
+        # })
+
+        url = self.url['basket_list'].format(
+            self.host, self.profile['lang'], self.initial_time,
+            'false', self.get_time(), -1, 1, '', 'asc'
+        )
+        self.debug and print(f"[DEBUG] {url}")
+
+        resp = self.session.get(url, headers = {
+            'Referer': self.referer,
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,und;q=0.6'
+        })
+        self.basket_list = resp.json()
+        
+        self.debug and print(self.basket_list)
+
+        # 목록 출력
+        print(f"[+] 소망가방 목록")
+        for row in self.basket_list['rows']:
+            print(f"    [-] {row['gwamok_kname']} - {row['prof_name']}")
+
+        return self.basket_list
+    
+    def load_sugang_list(self):
+        '''
+        기 수강신청된 리스트를 가져오는 함수
+        '''
+        url = self.url['sugang_list'].format(
+            self.host, self.profile['lang'], self.initial_time,
+            'false', self.get_time(), -1, 1, '', 'asc'
+        )
+        self.debug and print(f"[DEBUG] {url}")
+
+        resp = self.session.get(url, headers = {'Referer': self.referer})
+        self.sugang_list = resp.json()
+        
+        self.debug and print(self.basket_list)
+
+        # 목록 출력
+        print(f"[+] 수강신청 목록")
+        for row in self.sugang_list['rows']:
+            print(f"    [-] {row['gwamok_kname']} - {row['prof_name']}")
+
+        return self.basket_list
+    
+
+    def reqeust_all(self):
+        '''
+        소망가방 리스트에 있는 과목들을 일괄 신청하는 함수
+        '''
+        already_sugang_list = []
+
+        # 이미 수강중인 과목은 삭제
+        for row in self.sugang_list['rows']:
+            if row["wait_no"] == 0:
+                already_sugang_list.append(row['gyoyuk_haksu'].split('-')[1])
+
+
+        # run!!
+        print('[+] 수강신청 시작')
+        for row in self.basket_list['rows']:
+            if row['haksu_cd'] in already_sugang_list:
+                continue
+            url = self.url['sugang_mode'].format(
+                self.host, self.profile['lang'], self.initial_time, 'insert', self.get_time()
+            )
+            self.debug and print(f"[DEBUG] {url}")
+            request_subject = {
+                'params': row['params'],
+                'retake_yn': 'N' #row['retake_yn']
+            }
+            self.debug and print(f"[DEBUG] {request_subject}")
+
+            resp = self.session.post(url, data = request_subject, headers = {'Referer': self.referer})
+            result = resp.json()
+            print(f"    [+] {row['gwamok_kname']}")
+            print(f"        [-] {result['msg']}")
+
+        return
+
+    def get_time(self, forward = 0):
+        return int((time.time() - forward) * 1000)
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            print('[*] =============== macro start =============== ')
+
+            macro = Macro()
+
+            if macro.login():
+                macro.load_basket_list()
+                macro.load_sugang_list()
+                macro.reqeust_all()
+
+            print('[*] =============== macro end =============== ')
+            break
+        except Exception as e:
+            print('[!] =============== macro error =============== ')
+            print(e)
+            
